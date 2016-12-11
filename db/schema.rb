ab66939879f5eeb627829216d75d0ef360ae93a5
Victoria Moser <moservictoria02@gmail.com>
2016-12-10 21:54:55 -0500

    added media model, added paperclip to app, added media to article model, updated schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161210235133) do
+ActiveRecord::Schema.define(version: 20161210234210) do
 
   create_table "articles", force: :cascade do |t|
     t.text     "body"
     t.string   "title"
     t.string   "url"
     t.date     "pub"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-    t.integer  "category_id"
-    t.index ["category_id"], name: "index_articles_on_category_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "categories", force: :cascade do |t|
