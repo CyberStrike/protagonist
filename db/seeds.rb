7cc6b68b1a5ff6eccd36e80e00ef74538eadc5f1
Susan Davies <sdav9375@gmail.com>
2016-12-10 22:51:04 -0500

    fix seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,43 +6,43 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Article.create!([{
+articles = Article.create!([{
   title:   "A Tech Story",
   body:   "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation",
   url:   "#",
-  date:   "",
+  pub:   "12/10/16",
   category_id: "1"
 },
 {
   title:   "A Science Story",
   body:   "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation",
   url:   "#",
-  date:   "",
+  pub:   "12/10/16",
   category_id: "2"
 },
 {
   title:   "A Business Story",
   body:   "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation",
   url:   "#",
-  date:   "",
+  pub:   "12/10/16",
   category_id: "3"
 },
 {
   title:   "A Law Story",
   body:   "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation",
   url:   "#",
-  date:   "",
+  pub:   "12/10/16",
   category_id: "4"
 },
 {
   title:   "A Sports Story",
   body:   "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation",
   url:   "#",
-  date:   "",
+  pub:   "12/10/16",
   category_id: "5"
 }])
 
-Category.create!([{
+categories = Category.create!([{
   title:  "Technology"
 },
 {
