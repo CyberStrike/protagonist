a1d03afef7a0c1634c9092f21ce15c8657cfdc07
Susan Davies <sdav9375@gmail.com>
2016-12-10 22:00:03 -0500

    more views and seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,55 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Article.create!([{
+  title:   "A Tech Story",
+  body:   "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation",
+  url:   "#",
+  date:   "",
+  category_id: "1"
+},
+{
+  title:   "A Science Story",
+  body:   "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation",
+  url:   "#",
+  date:   "",
+  category_id: "2"
+},
+{
+  title:   "A Business Story",
+  body:   "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation",
+  url:   "#",
+  date:   "",
+  category_id: "3"
+},
+{
+  title:   "A Law Story",
+  body:   "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation",
+  url:   "#",
+  date:   "",
+  category_id: "4"
+},
+{
+  title:   "A Sports Story",
+  body:   "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation",
+  url:   "#",
+  date:   "",
+  category_id: "5"
+}])
+
+Category.create!([{
+  title:  "Technology"
+},
+{
+  title: "Science"
+},
+{
+  title: "Business"
+},
+{
+  title: "Law"
+},
+{
+  title: "Sports"
+}])
