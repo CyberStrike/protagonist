9f86c8f9ed29b79ada1771f568750c94af423b20
Chris Scott <cyberstrike@gmail.com>
2016-12-12 14:14:22 -0500

    fixed routes and remove links to edit

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  resources :categories
-  resources :articles
-
   root 'articles#index'
+  resources :articles, only: [:index, :show]
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
