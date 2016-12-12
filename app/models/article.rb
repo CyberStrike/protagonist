0f056b6a1d2585daa07b9884f3a5414eeb699b37
Victoria Moser <moservictoria02@gmail.com>
2016-12-12 14:27:28 -0500

    trying to fix the mobil design with Denise

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,6 +3,7 @@ class Article < ApplicationRecord
   has_many :media, class_name: 'Media'
   accepts_nested_attributes_for :media, :reject_if => :all_blank, :allow_destroy => true
 
+  #to be able to scroll between the articles (loop?)
   def next
     self.class.where("id > ?", id).first
   end
